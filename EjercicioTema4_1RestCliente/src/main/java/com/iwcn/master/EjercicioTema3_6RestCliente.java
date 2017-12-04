87812cb7a4f4eec81a16542da037f12e13aaf3f1
Lorena-Martin <lorena.360.1994@gmail.com>
2017-12-04 21:32:25 +0100

    Ejercicios

---
+++ EjercicioTema4_1RestCliente/src/main/java/com/iwcn/master/EjercicioTema3_6RestCliente.java
@@ -0,0 +1,13 @@
+package com.iwcn.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioTema3_6RestCliente {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioTema3_6RestCliente.class, args);
+    }
+
+}
---
+++ EjercicioTema4_1RestServidor/src/test/java/com/iwcn/master/principal/PrincipalTest.java
@@ -0,0 +1,15 @@
+package com.iwcn.master.principal;
+
+import org.junit.Test;
+
+import com.iwcn.master.EjercicioTema3_6RestServidor;
+
+public class PrincipalTest {
+
+	@Test
+	public void principalTest() {
+		String[] string = {"prueba"};
+		EjercicioTema3_6RestServidor.main(string);
+	}
+
+}
